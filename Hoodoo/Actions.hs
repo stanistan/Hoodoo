781d62a30f03bd7b4fd591df6dbfdc25f5eb7484
Stan Rozenraukh <stan@stanistan.com>
2013-04-21 08:56:45 -0400

    Some cleanup

--- Hoodoo/Actions.hs
+++ Hoodoo/Actions.hs
@@ -1,14 +1,11 @@
-module Hoodoo.Actions
-
-where
+module Hoodoo.Actions where
 
 import Hoodoo.Todo
 
 type Args = [String]
+type Handler = ([Todo] -> Args -> ([Todo], [Todo]))
 
-data Action = Action { triggers :: [String]
-                     , fn :: ([Todo] -> Args -> ([Todo], [Todo]))
-                     }
+data Action = Action { triggers :: [String], fn :: Handler }
 
 actions = [ (Action ["", "la"] listAll)
           , (Action ["list-done", "ld"] listDone)
@@ -18,40 +15,41 @@ actions = [ (Action ["", "la"] listAll)
           , (Action ["clean", "c"] clean)
           ]
 
-actionName :: String -> Action -> Bool
-actionName name action = elem name $ triggers action
-
-findByName name = takeAction $ filter (actionName name) actions
-
-takeAction :: [Action] -> Action
-takeAction (x:[]) = x
-takeAction [] = head actions
-
-
 dispatch :: String -> Args -> ([Todo], [Todo])
 dispatch content [] = dispatch content [""]
 dispatch content (name:args) = (fn action) ts args
   where action = findByName name
         ts = map todoFromString $ lines content
 
-listAll :: [Todo] -> Args -> ([Todo], [Todo])
+listAll :: Handler
 listAll todos _ = (todos, todos)
 
-listDone :: [Todo] -> Args -> ([Todo], [Todo])
+listDone :: Handler
 listDone todos _ = (keepFinished todos, todos)
 
-listLeft :: [Todo] -> Args -> ([Todo], [Todo])
+listLeft :: Handler
 listLeft todos _ = (removeFinished todos, todos)
 
-addTask :: [Todo] -> Args -> ([Todo], [Todo])
+addTask :: Handler
 addTask ts args = listAll todos args
   where todos = reorderTodos $ addTodo ts (head args)
 
-markDone :: [Todo] -> Args -> ([Todo], [Todo])
+markDone :: Handler
 markDone ts args = listAll todos args
   where todos = markAsFinished ts (read (head args) :: Int)
 
-clean :: [Todo] -> Args -> ([Todo], [Todo])
+clean :: Handler
 clean ts args = listAll todos args
   where todos = reorderTodos $ removeFinished ts
 
+-- Action helpers
+
+actionName :: String -> Action -> Bool
+actionName name action = elem name $ triggers action
+
+findByName :: String -> Action
+findByName name = takeAction $ filter (actionName name) actions
+
+takeAction :: [Action] -> Action
+takeAction (x:[]) = x
+takeAction [] = head actions
